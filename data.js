e9beb3f7cd0f50e392a812ed1227bc5c38852be2
Riickard <rickard.englund10@outlook.com>
2022-12-21 11:41:25 +0100

    changed data.js

--- data.js
+++ data.js
@@ -1,69 +1,106 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: '172',
-        mass: '77',
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: '202',
-        mass: '136',
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: '150',
-        mass: '49',
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: '188',
-        mass: '84',
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 
-
 const products = [
-    
-]
-
+  {
+    id: 1,
+    title: "Iphone 9",
+    price: 8998,
+    inStock: true,
+    totalAmount: 99,
+    discount: true,
+  },
+  {
+    id: 2,
+    title: "Iphone 10",
+    price: 9998,
+    inStock: true,
+    totalAmount: 5,
+    discount: true,
+  },
+  {
+    id: 3,
+    title: "Iphone 11",
+    price: 10800,
+    inStock: false,
+    totalAmount: 0,
+    discount: false,
+  },
+  {
+    id: 4,
+    title: "Iphone 12",
+    price: 11500,
+    inStock: true,
+    totalAmount: 58,
+    discount: true,
+  },
+  {
+    id: 5,
+    title: "Iphone 13",
+    price: 12998,
+    inStock: true,
+    totalAmount: 99,
+    discount: false,
+  },
+];
 
 const users = [
-    {
-        id: 1,
-        name: "Leia",
-        isOnline: false,
-        age: 25
-    },
-    {
-        id: 2,
-        name: "Look",
-        isOnline: true,
-        age: 36
-    },
-    {
-        id: 3,
-        name: "James",
-        isOnline: true,
-        age: 35
-    },
-    {
-        id: 4,
-        name: "Brook",
-        isOnline: false,
-        age: 32
-    },
-    {
-        id: 5,
-        name: "Havana",
-        isOnline: false,
-        age: 29
-    }
-]
+  {
+    id: 1,
+    name: "Leia",
+    isOnline: false,
+    age: 25,
+  },
+  {
+    id: 2,
+    name: "Look",
+    isOnline: true,
+    age: 36,
+  },
+  {
+    id: 3,
+    name: "James",
+    isOnline: true,
+    age: 35,
+  },
+  {
+    id: 4,
+    name: "Brook",
+    isOnline: false,
+    age: 32,
+  },
+  {
+    id: 5,
+    name: "Havana",
+    isOnline: false,
+    age: 29,
+  },
+];
